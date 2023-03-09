e96b390015a49de24fe22a6f6bc59bf2da765da5
selife <sundaycatgo@gmail.com>
2023-03-09 19:29:49 +0400

    first commit

--- app/src/main/java/com/example/sss/MainActivity.kt
+++ app/src/main/java/com/example/sss/MainActivity.kt
@@ -5,23 +5,14 @@ import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.sss.ui.theme.SssTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+
+
 
 class MainActivity : ComponentActivity() {
     private lateinit var photoRecyclerView: RecyclerView
@@ -44,7 +35,7 @@ class MainActivity : ComponentActivity() {
 
         GlobalScope.launch(Dispatchers.IO) {
             try {
-                val photos = unsplashService.getPhotos("your_client_id", page = 1, perPage = 10)
+                val photos = UnsplashService.getPhotos("rUvu4QNu8rP_wvTNjQB-ikSPLURJi1ReZ6de-VBdhck", page = 1, perPage = 10)
                 withContext(Dispatchers.Main) {
                     photoRecyclerView.adapter = PhotoAdapter(photos)
                     progressBar.visibility = View.GONE
--- app/src/main/java/com/example/sss/PhotoAdapter.kt
+++ app/src/main/java/com/example/sss/PhotoAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.myapp.models.Photo
+import com.bumptech.glide.request.RequestOptions
 
 class PhotoAdapter(private val context: Context, private val photos: List<Photo>) :
     RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
